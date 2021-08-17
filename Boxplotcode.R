a83fba72eccf249189fac07bc460ee6f3b0cd6b5
Megan Feddern <mfeddern@uw.edu>
2021-08-17 12:51:15 -0700

    adding y axis to figure

--- Boxplotcode.R
+++ Boxplotcode.R
@@ -24,12 +24,12 @@ nord.aur<-nord(palette="aurora", 5) #assigning colors from the nord package
 nord.fro<-nord(palette="frost", 1) # adding blue to the aurora package from the frost package
 col <- c(nord.aur, nord.fro)
 
-pdf(file="Samplesizefigure.pdf", width=10, height=5)
+pdf(file="Samplesizefigure.pdf", width=11, height=5)
 
 mat <- matrix(data = c(1,2,3, 4,5), nrow=1) #the multi-panel component of the map is assigned using 
                                                           #layout, you could use par instead. Right now it is set up for a 3X2
 
-par(oma=c(1,0,0,0),mar=c(6, 3, 2, 2), family = 'sans')  #adjusting margins
+par(oma=c(1,2,0,0),mar=c(6, 3, 2, 2), family = 'sans')  #adjusting margins
 lay <- layout(mat=mat,  heights=c(1,1), widths=c(1,1,1,1,1)) #this assigns the width/height of each panel if you want to adjust it
 #layout.show(6) check what your layout looks like
 
@@ -49,6 +49,8 @@ barplot(solen$fishcount, ylab = '', xlab="", col=col[5],
 rasterImage(solepic, 2,10.5,8.5,11.5, interpolate = TRUE) #this adds the png to the plot -- you may need to play around with the numbers because it can skew teh dimensions depending on the png shape
 text(5.25,10, label = 'SL = 129 ± 20 (mm)', cex=1.1)
 text(5.25,9.5, label = 'n = 42', cex=1.1)
+mtext('Number of fish sampled', side = 2, line = 0, outer = TRUE, at = NA,
+      adj = NA, padj = NA, cex = 1.1)
 
 
 barplot(rockn$fishcount, ylab = "", xlab="", col= col[6], 
@@ -81,10 +83,6 @@ rasterImage(herringpic, 2.5,18.5,9,19.25, interpolate = TRUE)
 text(5.75,17, label = 'SL = 141 ± 33 (mm)', cex=1.1)
 text(5.75,16.25, label = 'n = 52', cex=1.1)
 
-#barplot(EG$Total, ylab = "", xlab="", col= col[1], 
-        #ylim = c(0,20), main = "F. Eastern Stock", cex.lab=1, cex.axis = 1.25, 
-        #cex.names = 1.25, cex.main=1.25, las=2, names.arg = EG$Decade)
-#rasterImage(sl.eg, 3,13,5.5,18, interpolate = TRUE)
 
 dev.off()
 
