825e6897ba34bd29daa301c0f35c21fbf1035563
rlwelicky <rwelicky@uw.edu>
2021-08-23 15:39:53 -0700

    final color changes

--- Boxplotcode.R
+++ Boxplotcode.R
@@ -21,9 +21,9 @@ library(inauguration)
 summ<-read.csv("SLSumm.csv", header=TRUE)
 
 
-pdf(file="Samplesizefigure.pdf", width=8.5, height=11)
+pdf(file="Samplesizefigure1.pdf", width=8.5, height=11)
 
-<<<<<<< HEAD
+
 mat <- matrix(data = c(1,2,3,4,
                         5, 6,7), ncol = 7) #the multi-panel component of the map is assigned using 
                                                           #layout, you could use par instead. Right now it is set up for a 3X2
@@ -33,7 +33,7 @@ lay <- layout(mat=mat,  heights=c(0.5,0.5,0.5,0.5,0.5), widths=c(70,70,70,70,70)
 #layout.show(6) check what your layout looks like
 
 
-=======
+
 pdf(file="Samplesizefigure.pdf", width=11, height=5)
 
 mat <- matrix(data = c(1,2,3, 4,5), nrow=1) #the multi-panel component of the map is assigned using 
@@ -45,7 +45,7 @@ lay <- layout(mat=mat,  heights=c(1,1), widths=c(1,1,1,1,1)) #this assigns the w
 
 
 
->>>>>>> 81df1a09cc08003ef54638e187054bfbaa5b348c
+
 library(png)
 solepic <- readPNG("solepic.png") #this imports the silhouettes as a png
 hakepic<- readPNG("hakepic.png")
@@ -54,48 +54,43 @@ herringpic <- readPNG("herringpic.png")
 rockpic <- readPNG("rockpic.png")
 
 
-<<<<<<< HEAD
+
 
 barplot(solen$fishcount, ylab = "", xlab="", col="#5445b1", 
-        names.arg = solen$decade, ylim = c(0,12), main = "English Sole", cex.lab=2, cex.axis = 2, 
-        cex.names = 2, cex.main=2, las=2)
-#rasterImage(solepic, 0.5,12,3,10.5, interpolate = TRUE) #this adds the png to the plot -- you may need to play around with the numbers because it can skew teh dimensions depending on the png shape
-#mtext('Sample Size', side = 2, line = 0, outer = FALSE, at = NA,
-      #adj = NA, padj =0, cex = 1.1)
+     names.arg = solen$decade, ylim = c(0,12), main = "English Sole", cex.lab=2, cex.axis = 2, 
+      cex.names = 2, cex.main=2, las=2)
+rasterImage(solepic, 0.5,12,3,10.5, interpolate = TRUE) #this adds the png to the plot -- you may need to play around with the numbers because it can skew teh dimensions depending on the png shape
+mtext('Sample Size', side = 2, line = 0, outer = FALSE, at = NA,
+      adj = NA, padj =0, cex = 1.1)
 
-barplot(rockn$fishcount, ylab = "", xlab="", col= "#749dae", 
-        names.arg = rockn$decade,ylim = c(0,12), main = "Rockfish", cex.lab=2, 
-        cex.axis = 2, cex.names = 2, cex.main=2, las=2)
-#rasterImage(rockpic, 0.5,12,3,10.5, interpolate = TRUE)
+barplot(rockn$fishcount, ylab = "", xlab="", col= "#0077b6", 
+     names.arg = rockn$decade,ylim = c(0,12), main = "Rockfish", cex.lab=2, 
+      cex.axis = 2, cex.names = 2, cex.main=2, las=2)
+rasterImage(rockpic, 0.5,12,3,10.5, interpolate = TRUE)
 
 
 barplot(haken$fishcount, ylab = "", xlab="", col= "#f3c483", 
-        names.arg = haken$decade, ylim = c(0,12), main = "Pacific Hake", cex.lab=1, cex.axis = 2, 
-        cex.names = 2, cex.main=2, las=2)
-#rasterImage(hakepic, 0.5,12,3,10.5, interpolate = TRUE)
-#mtext('Decade', side = 1, line = 0, outer = FALSE, at = NA,
-      #adj = 0, padj = NA, cex = 1.1)
+     names.arg = haken$decade, ylim = c(0,12), main = "Pacific Hake", cex.lab=1, cex.axis = 2, 
+      cex.names = 2, cex.main=2, las=2)
+rasterImage(hakepic, 0.5,12,3,10.5, interpolate = TRUE)
+mtext('Decade', side = 1, line = 0, outer = FALSE, at = NA,
+      adj = 0, padj = NA, cex = 1.1)
 
 
 barplot(pollockn$fishcount, ylab = "", xlab="", col= "#5c1a33", 
-         names.arg = pollockn$decade, ylim = c(0,12), main = "Walleye Pollock", cex.lab=1, cex.axis = 2, 
-        cex.names = 2, cex.main=2, las=2)
-#rasterImage(pollockpic, 0.5,12,3,10.5, interpolate = TRUE)
+        names.arg = pollockn$decade, ylim = c(0,12), main = "Walleye Pollock", cex.lab=1, cex.axis = 2, 
+      cex.names = 2, cex.main=2, las=2)
+rasterImage(pollockpic, 0.5,12,3,10.5, interpolate = TRUE)
 
 
 barplot(herringn$fishcount, ylab = "", xlab="", col= "#cd3341",
-        names.arg = herringn$decade, ylim = c(0,14), main = "Pacific Herring", cex.lab=2, 
-        cex.axis = 2, cex.names = 2, cex.main=2, las=2)
-#rasterImage(herringpic, 0.5,12.5,3,12, interpolate = TRUE)
+       names.arg = herringn$decade, ylim = c(0,14), main = "Pacific Herring", cex.lab=2, 
+      cex.axis = 2, cex.names = 2, cex.main=2, las=2)
+rasterImage(herringpic, 0.5,12.5,3,12, interpolate = TRUE)
 
 
 
-#barplot(EG$Total, ylab = "", xlab="", col= col[1], 
-        #ylim = c(0,20), main = "F. Eastern Stock", cex.lab=1, cex.axis = 2, 
-        #cex.names = 2, cex.main=2, las=2, names.arg = EG$Decade)
-#rasterImage(sl.eg, 3,13,5.5,18, interpolate = TRUE)
-=======
-barplot(solen$fishcount, ylab = '', xlab="", col=col[5], 
+barplot(solen$fishcount, ylab = '', xlab="", col="#5445b1", 
         names.arg = solen$decade, ylim = c(0,12), main = "English Sole", cex.lab=1.5, cex.axis = 1.25, 
         cex.names = 1.25, cex.main=1.25, las=2)
 rasterImage(solepic, 2,10.5,8.5,11.5, interpolate = TRUE) #this adds the png to the plot -- you may need to play around with the numbers because it can skew teh dimensions depending on the png shape
@@ -105,14 +100,14 @@ mtext('Number of fish sampled', side = 2, line = 0, outer = TRUE, at = NA,
       adj = NA, padj = NA, cex = 1.1)
 
 
-barplot(rockn$fishcount, ylab = "", xlab="", col= col[6], 
+barplot(rockn$fishcount, ylab = "", xlab="", col= "#0077b6", 
         names.arg = rockn$decade,ylim = c(0,12), main = "Rockfish", cex.lab=1.5, 
         cex.axis = 1.25, cex.names = 1.25, cex.main=1.25, las=2)
 rasterImage(rockpic, 2.5,10.5,9.5,11.5, interpolate = TRUE)
 text(6,10, label = 'SL = 191 ± 36 (mm)', cex=1.1)
 text(6,9.5, label = 'n = 36', cex=1.1)
 
-barplot(haken$fishcount, ylab = "", xlab="", col= col[4], 
+barplot(haken$fishcount, ylab = "", xlab="", col= "#f3c483", 
         names.arg = haken$decade, ylim = c(0,14), main = "Pacific Hake", cex.lab=1, cex.axis = 1.25, 
         cex.names = 1.25, cex.main=1.25, las=2)
 rasterImage(hakepic,1,12.5,7.5,13.5, interpolate = TRUE)
@@ -121,21 +116,20 @@ text(4.25,11.5, label = 'n = 42', cex=1.1)
 mtext('Decade', side = 1, line = 5, outer = FALSE, at = NA,
       adj = NA, padj = NA, cex = 1.1)
 
-barplot(pollockn$fishcount, ylab = "", xlab="", col= col[3], 
+barplot(pollockn$fishcount, ylab = "", xlab="", col= "#5c1a33", 
          names.arg = pollockn$decade, ylim = c(0,14), main = "Walleye Pollock", cex.lab=1, cex.axis = 1.25, 
         cex.names = 1.25, cex.main=1.25, las=2)
 rasterImage(pollockpic, 2.5,12.5,9,13.5, interpolate = TRUE)
 text(5.75,12, label = 'SL = 133 ± 56 (mm)', cex=1.1)
 text(5.75,11.5, label = 'n = 50', cex=1.1)
 
-barplot(herringn$fishcount, ylab = "", xlab="", col= col[2],
+barplot(herringn$fishcount, ylab = "", xlab="", col= "#cd3341",
         names.arg = herringn$decade, ylim = c(0,20), main = "Pacific Herring", cex.lab=1.5, 
         cex.axis = 1.25, cex.names = 1.25, cex.main=1.25, las=2)
 rasterImage(herringpic, 2.5,18.5,9,19.25, interpolate = TRUE)
 text(5.75,17, label = 'SL = 141 ± 33 (mm)', cex=1.1)
 text(5.75,16.25, label = 'n = 52', cex=1.1)
 
->>>>>>> 81df1a09cc08003ef54638e187054bfbaa5b348c
 
 dev.off()
 
